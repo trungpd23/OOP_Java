f64b7611bb77a57af1340859a606dd80e80b107f
trungpd23 <phamdinhtrung102@gmail.com>
2025-02-21 09:11:49 +0700

    FirstCode

---
+++ ChatServer.java
@@ -0,0 +1,58 @@
+package ChattingSoftware;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+    private static List<ClientHandler> clients = new ArrayList<>();
+  
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(5000);
+        System.out.println("Server started. Waiting for clients...");
+  
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket);
+  
+            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+            clients.add(clientThread);
+            new Thread(clientThread).start();
+        }
+    }
+  }
+  
+  class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private List<ClientHandler> clients;
+    private PrintWriter out;
+    private BufferedReader in;
+  
+    public ClientHandler(Socket socket, List<ClientHandler> clients) throws IOException {
+        this.clientSocket = socket;
+        this.clients = clients;
+        this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+  
+    public void run() {
+        try {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                for (ClientHandler aClient : clients) {
+                    aClient.out.println(inputLine);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        } finally {
+            try {
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
